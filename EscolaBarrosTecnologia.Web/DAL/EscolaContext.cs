60c34798f288d4f5c7c2d36d88caced5f5213884
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 22:41:31 -0300

    Contruir a definição de propriedades a serem utilizadas pelo Entity Framework

---
+++ EscolaBarrosTecnologia.Web/DAL/EscolaContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EscolaBarrosTecnologia.Web.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EscolaBarrosTecnologia.Web.DAL
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext() :  base("EscolaBarrosConexao")
+        {
+
+        }
+
+        //Definição das classes que se tornarão tabelas no database
+        public DbSet<Estudantes> Estudante { get; set; }
+        public DbSet<Matriculas> Matricula { get; set; }
+        public DbSet<Cursos> Curso { get; set; }
+
+
+        //Método que define a propriedade que desabilita a convenção de pluralização do Entity Framework
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
